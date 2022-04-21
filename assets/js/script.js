836c4e8196c9ab54b55ea93278878fcffe64aec4
elvinkyungu <elvinkyungu.75@gmail.com>
2022-04-21 11:44:10 +0200

    insert others pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,28 +5,6 @@ var slides = document.querySelectorAll('.slide');
 var btns = document.querySelectorAll('.btn');
 let currentSlide = 1;
         // Partie about
-let preveiwContainer = document.querySelector('.products-preview');
-let previewBox = preveiwContainer.querySelectorAll('.preview');
-
-document.querySelectorAll('.products-container .product').forEach(product =>{
-  product.onclick = () =>{
-    preveiwContainer.style.display = 'flex';
-    let name = product.getAttribute('data-name');
-    previewBox.forEach(preview =>{
-      let target = preview.getAttribute('data-target');
-      if(name == target){
-        preview.classList.add('active');
-      }
-    });
-  };
-});
-
-previewBox.forEach(close =>{
-  close.querySelector('.fa-times').onclick = () =>{
-    close.classList.remove('active');
-    preveiwContainer.style.display = 'none';
-  };
-});
 
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
@@ -37,9 +15,6 @@ menu.addEventListener('click', () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 });
-
-
-        
 // Javascript for image slider manual navigation
 var manualNav = function(manual){
 slides.forEach((slide) => {
